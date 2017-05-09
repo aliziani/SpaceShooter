0681f3b2476acbd9a1eb9212e6364d321a1fc052
Ziani Ali <ali.ziani@digitaslbi.com>
2017-05-09 16:07:08 +0200

    lot of stuff, selection menu, bug fixing, progressive waves

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour {
 
     public GameObject[] hazards;
     public Vector3 spawnValues;
-    public int hazardCount;
+    //public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
@@ -20,6 +20,10 @@ public class GameController : MonoBehaviour {
 
     private bool gameOver;
     private bool restart;
+    private int currentTurn;
+
+
+    private static string KeyNameSpaceShipSelected = "SpaceShip.Selected.Name";
 
     IEnumerator SpawnWaves()
     {
@@ -27,6 +31,10 @@ public class GameController : MonoBehaviour {
         yield return new WaitForSeconds(startWait);
         while (true)
         {
+            HideTurn();
+
+            //int hazardCount = (int)Mathf.Log(currentTurn, 2f);
+            int hazardCount = currentTurn;
             for (int i = 0; i < hazardCount; i++)
             {
                 var hazard = hazards[Random.Range(0, hazards.Length)];
@@ -36,17 +44,49 @@ public class GameController : MonoBehaviour {
                 // [0,1] determines how much asteroids we want to show by seconds
                 yield return new WaitForSeconds(spawnWait);
             }
+
+            DisplayTurn();
+
             // wait before next wave
             yield return new WaitForSeconds(waveWait);
-
+            
             if (gameOver)
             {
                 restartText.text = "Press 'R' for Restart";
                 restart = true;
                 break;
             }
+            currentTurn++;
         }
     }
+
+
+    //IEnumerator SpawnWaves()
+    //{
+    //    // wait before starting
+    //    yield return new WaitForSeconds(startWait);
+    //    while (true)
+    //    {
+    //        for (int i = 0; i < hazardCount; i++)
+    //        {
+    //            var hazard = hazards[Random.Range(0, hazards.Length)];
+    //            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0, spawnValues.z);
+    //            Quaternion spawnRotation = Quaternion.identity;
+    //            Instantiate(hazard, spawnPosition, spawnRotation);
+    //            // [0,1] determines how much asteroids we want to show by seconds
+    //            yield return new WaitForSeconds(spawnWait);
+    //        }
+    //        // wait before next wave
+    //        yield return new WaitForSeconds(waveWait);
+
+    //        if (gameOver)
+    //        {
+    //            restartText.text = "Press 'R' for Restart";
+    //            restart = true;
+    //            break;
+    //        }
+    //    }
+    //}
 
     private void Update()
     {
@@ -66,7 +106,11 @@ public class GameController : MonoBehaviour {
         restart = false;
         restartText.text = string.Empty;
         gameOverText.text = string.Empty;
+        currentTurn = 1;
+
         UpdateScore();
+        // Create the right space ship for the player
+        Instantiate(Resources.Load(PlayerPrefs.GetString(KeyNameSpaceShipSelected), typeof(GameObject))); // TODO: bad and you should feel bad !
         // to not block the main process
         StartCoroutine(SpawnWaves());
     }
@@ -82,6 +126,15 @@ public class GameController : MonoBehaviour {
         scoreText.text = "Score: " + score;
     }
 
+    void DisplayTurn()
+    {
+        gameOverText.text = "Turn: " + currentTurn;
+    }
+
+    void HideTurn()
+    {
+        gameOverText.text = string.Empty;
+    }
 
     public void GameOver()
     {
---
+++ Space Shooter/Assets/Scripts/LoaderManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoaderManager : MonoBehaviour {
+
+    public List<GameObject> spaceShipsPlayer;
+    public Button NextButton;
+    public Button PreviousButton;
+    public Button PlayButton;
+
+    private static string KeyNameSpaceShipSelected = "SpaceShip.Selected.Name";
+    private string      spaceShipName;
+    private int         spaceShipIndex;
+    private GameObject  currentSpaceShip;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Starting...");
+        if (spaceShipsPlayer.Count > 0)
+        {
+            selectSpaceShip(0);
+        }else
+        {
+            Debug.LogError("No spaceships in the queue !");
+        }
+        NextButton.onClick.AddListener(GoNext);
+        PreviousButton.onClick.AddListener(GoPrevious);
+        PlayButton.onClick.AddListener(Play);
+    }
+
+    void GoNext()
+    {
+        spaceShipIndex = (spaceShipIndex == (spaceShipsPlayer.Count - 1)) ? 0 : spaceShipIndex + 1;
+        selectSpaceShip(spaceShipIndex);
+    }
+    void GoPrevious()
+    {
+        spaceShipIndex = (spaceShipIndex == 0) ? (spaceShipsPlayer.Count - 1) : spaceShipIndex - 1;
+        selectSpaceShip(spaceShipIndex);
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    
+    void selectSpaceShip(int newIndex)
+    {
+        if (currentSpaceShip != null)
+        {
+            Debug.Log("Destroy old spaceship: " + currentSpaceShip.name);
+            Destroy(currentSpaceShip);
+        }
+        spaceShipName = spaceShipsPlayer[spaceShipIndex].name;
+        currentSpaceShip = Instantiate(spaceShipsPlayer[spaceShipIndex], new Vector3(0, 0, -0.5f), Quaternion.identity);
+        Debug.Log("Create new spaceship: " + currentSpaceShip.name);
+        var keyStored = String.Format("Prefabs/SpaceShips/Players/Player_{0}", spaceShipName);
+        PlayerPrefs.SetString(KeyNameSpaceShipSelected, keyStored);
+        Debug.Log("Save name in PlayerPrefs: " + keyStored);
+    }
+
+}
